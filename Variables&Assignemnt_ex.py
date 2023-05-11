d6dfab76aa5f3592de7f2c44b4c1d2a3ac159277
SashaSma <alexandra.smarzova@novanta.com>
2023-05-11 15:52:04 +0200

    ii

---
+++ Variables&Assignemnt_ex.py
@@ -0,0 +1,22 @@
+addition = (4+5)
+#create a variable and assign it the sum of two integers
+summed = 4 + 5
+#create a variable and assign it the difference of two integers
+difference = 5 - 4
+#create a variable and assign it the result of one integer being divided by another integer
+divided = 6 / 3
+#create a variable and assign it the product of two integers
+product = 4 * 5
+#create a variable and assign it the result of 3 raised to the 8th power
+exponent = 3 ** 8
+#create a variable and assign it the result of 10 floor division 3
+result2 = 10 // 3
+#create a variable and assign it the integer 2 using the expression 17 modulo
+integer = 17 % 15
+
+var = "ahoj"
+print(var)
+print("ahoj")
+print(5+5)
+
+
