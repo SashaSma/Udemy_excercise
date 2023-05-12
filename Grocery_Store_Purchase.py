188257722ea79aea65016a423271f51bbed1360e
SashaSma <alexandra.smarzova@novanta.com>
2023-05-12 19:14:41 +0200

    cviceni z udemy

---
+++ Grocery_Store_Purchase.py
@@ -0,0 +1,21 @@
+"""A customer of a grocery store is purchasing 6 items.
+The names and prices of the items are as follows:
+Penne 16 oz Pack of 12 - $16.68
+Arrabiata Pasta Sauce 24 oz - $6.98
+Bag of 20 Organic Garlic Cloves - $16.78
+Italian Seasoning 1.5 oz Bottle - $15.26
+Artisan Baguettes Twin Pack - $3.00
+12 oz Bag of Meatballs - $4.39
+In a .py file, write a program which calculates the subtotal of all 6 of these items
+using an expression.  The subtotal is just the sum of all of their prices.
+Use print() to display the result of the expression."""
+
+Penne = 16.68
+Arrabiata_Pasta_Sauce = 6.98
+Garlic_Gloves = 16.78
+Italian_Seasoning = 15.26
+Baguette = 3.00
+Meatballs = 4.39
+subtotal = (Penne+Arrabiata_Pasta_Sauce+Garlic_Gloves+Italian_Seasoning+Baguette+Meatballs)
+print(subtotal)
+
